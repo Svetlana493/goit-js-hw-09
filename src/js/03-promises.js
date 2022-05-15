c754430c39d67234ea6622a0aabae3f313c78a3d
Svetlana Fedorenko <svetlana493@ukr.net>
2022-05-15 12:02:48 +0300

    зробила

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    submitEl: document.querySelector('.form'),
+    delayEl: document.querySelector('[name="delay"]'),
+    stepEl: document.querySelector('[name="step"]'),
+    amountEl: document.querySelector('[name="amount"]'),
+
+}
+
+const { submitEl, delayEl, stepEl, amountEl } = refs;
+submitEl.addEventListener("submit", onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        console.log(delay)
+        const shouldResolve = Math.random() > 0.3;
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay)
+    });
+
+}
+
+
+function generatePromises(delay, step, amount) {
+    for (let i = 1; i <= amount; i += 1) {
+        createPromise(i, delay)
+            .then(({ position, delay }) => {
+                Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+                    timeout: 6000,
+                });
+            })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+                    timeout: 6000,
+                });
+            });
+        console.log(delay)
+        delay += step;
+    }
+}
+
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const delay = +delayEl.value;
+    const step = +stepEl.value;
+    const amount = +amountEl.value;
+    generatePromises(delay, step, amount);
 }
